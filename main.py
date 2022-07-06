44e5848d3462338e7fb6f794172ced37ffc49064
nhn1030 <shgusska12@gmail.com>
2022-07-06 17:14:18 +0900

    mod

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import imp
+from typing import ItemsView
+from wsgiref import headers
 import requests
-
+import re
+import csv
 from bs4 import BeautifulSoup
-amazon_result = requests.get("https://www.amazon.com/Best-Sellers-Electronics/zgbs/electronics/ref=zg_bs_pg_50?_encoding=UTF8&pg=2")
 
-amazon_soup = BeautifulSoup(amazon_result.text, "html.parser")
+url = "https://www.cultizm.com/kor/sale/?p=1"
+# headers = {"User-Agent" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"}
+res = requests.get(url)
+soup = BeautifulSoup(res.text, "html.parser")
 
-print(amazon_soup)
 
-dasdasdasdad
+items = soup.find("div", attrs={"class":re.compile("^listing--container") }) 
+
+infos = items.find("a", attrs={"class":re.compile("^listing--supplier-link cultizm-supplier-name") })  
+
+# print(infos)
+
+def save_to_file():
+    file =open("list.csv", mode="w")
+    print(file)
+    return
+
+save_to_file()
+    
+# for item in items:
+#     name = listing--supplier-link cultizm-supplier-name
+# (item.find("div", attrs={"class" : "gridItemRoot"}).get_text())
